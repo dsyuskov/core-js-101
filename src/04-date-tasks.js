8d188d4128b9a91abde303348619a419d884cdcb
int <yuskovss@gmail.com>
2019-12-02 23:50:41 +0400

    add

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,16 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = date.getFullYear();
+  if (year % 4 !== 0) {
+    return false;
+  } if (year % 100 !== 0) {
+    return true;
+  } if (year % 400 !== 0) {
+    return false;
+  }
+  return true;
 }
 
 
@@ -73,8 +81,19 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const timespan = endDate - startDate;
+  const hours = Math.floor(timespan / (3600 * 1000)) < 10 ? `0${Math.floor(timespan / (3600 * 1000))}` : Math.floor(timespan / (3600 * 1000));
+  const minuts = Math.floor((timespan - hours * (3600 * 1000)) / (60 * 1000)) < 10
+    ? `0${Math.floor((timespan - hours * (3600 * 1000)) / (60 * 1000))}` : Math.floor((timespan - hours * (3600 * 1000)) / (60 * 1000));
+  const sec = Math.floor((timespan - hours * (3600 * 1000) - minuts * (60 * 1000)) / 1000) < 10
+    ? `0${Math.floor((timespan - hours * (3600 * 1000) - minuts * (60 * 1000))
+    / 1000)}` : Math.floor((timespan - hours * (3600 * 1000) - minuts * (60 * 1000)) / 1000);
+  // eslint-disable-next-line max-len
+  const ms = Math.round(((timespan - hours * (3600 * 1000) - minuts * (60 * 1000)) / 1000 - sec) * 1000) < 10
+    ? `00${Math.round(((timespan - hours * (3600 * 1000) - minuts * (60 * 1000)) / 1000 - sec) * 1000)}`
+    : Math.round(((timespan - hours * (3600 * 1000) - minuts * (60 * 1000)) / 1000 - sec) * 1000);
+  return `${hours}:${minuts}:${sec}.${ms}`;
 }
 
 
